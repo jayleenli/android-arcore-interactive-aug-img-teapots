a033445670c4e3286858bfe43f901039f2fb0768
Jayleen Li <jayleenli17@gmail.com>
2021-02-04 03:05:36 -0800

    four cute teapots but they aint rotated

--- augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/rendering/AugmentedImageRenderer.java
+++ augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/rendering/AugmentedImageRenderer.java
@@ -39,43 +39,40 @@ public class AugmentedImageRenderer {
     Instead of the image frame render the teapot 4 times
     Because the maze should attach to the image, it makes sense to put the mazeRenderer inside the AugmentedImageRenderer class.
    */
-//  private final ObjectRenderer imageFrameUpperLeft = new ObjectRenderer();
-//  private final ObjectRenderer imageFrameUpperRight = new ObjectRenderer();
-//  private final ObjectRenderer imageFrameLowerLeft = new ObjectRenderer();
-//  private final ObjectRenderer imageFrameLowerRight = new ObjectRenderer();
+  private final ObjectRenderer teapotLeft = new ObjectRenderer();
+  private final ObjectRenderer teapotMidLeft = new ObjectRenderer();
+  private final ObjectRenderer teapotMidRight = new ObjectRenderer();
+  private final ObjectRenderer teapotRight = new ObjectRenderer();
 
-  private final ObjectRenderer mazeRenderer = new ObjectRenderer();
-
-
-
+//  private final ObjectRenderer mazeRenderer = new ObjectRenderer();
 
   public AugmentedImageRenderer() {}
 
   public void createOnGlThread(Context context) throws IOException {
-    mazeRenderer.createOnGlThread(
-            context, "models/Teapot.obj", "models/frame_base.png");
-    mazeRenderer.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+//    mazeRenderer.createOnGlThread(
+//            context, "models/Teapot.obj", "models/frame_base.png");
+//    mazeRenderer.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
 
 
-//    imageFrameUpperLeft.createOnGlThread(
-//        context, "models/frame_upper_left.obj", "models/frame_base.png");
-//    imageFrameUpperLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-//    imageFrameUpperLeft.setBlendMode(BlendMode.AlphaBlending);
-//
-//    imageFrameUpperRight.createOnGlThread(
-//        context, "models/frame_upper_right.obj", "models/frame_base.png");
-//    imageFrameUpperRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-//    imageFrameUpperRight.setBlendMode(BlendMode.AlphaBlending);
-//
-//    imageFrameLowerLeft.createOnGlThread(
-//        context, "models/frame_lower_left.obj", "models/frame_base.png");
-//    imageFrameLowerLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-//    imageFrameLowerLeft.setBlendMode(BlendMode.AlphaBlending);
-//
-//    imageFrameLowerRight.createOnGlThread(
-//        context, "models/frame_lower_right.obj", "models/frame_base.png");
-//    imageFrameLowerRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-//    imageFrameLowerRight.setBlendMode(BlendMode.AlphaBlending);
+      teapotLeft.createOnGlThread(
+              context, "models/Teapot.obj", "models/frame_base.png");
+      teapotLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+      //imageFrameUpperLeft.setBlendMode(BlendMode.AlphaBlending);
+
+      teapotMidLeft.createOnGlThread(
+              context, "models/Teapot.obj", "models/frame_base.png");
+      teapotMidLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+      //imageFrameUpperRight.setBlendMode(BlendMode.AlphaBlending);
+
+      teapotMidRight.createOnGlThread(
+              context, "models/Teapot.obj", "models/frame_base.png");
+      teapotMidRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+      //imageFrameLowerLeft.setBlendMode(BlendMode.AlphaBlending);
+
+      teapotRight.createOnGlThread(
+              context, "models/Teapot.obj", "models/frame_base.png");
+      teapotRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+      //imageFrameLowerRight.setBlendMode(BlendMode.AlphaBlending);
   }
 
   public void draw(
@@ -87,71 +84,87 @@ public class AugmentedImageRenderer {
       float[] tintColor =
           convertHexToColor(TINT_COLORS_HEX[augmentedImage.getIndex() % TINT_COLORS_HEX.length]);
 
-      final float maze_edge_size = 132113.73f; // Magic number of maze size
+      //JAYLEEN: From the tutorial to load in custom obj. Reason for  this is because a lot of models are not centered at origin
+
+//      final float maze_edge_size = 132113.73f; // Magic number of maze size
+//      final float max_image_edge = Math.max(augmentedImage.getExtentX(), augmentedImage.getExtentZ()); // Get largest detected image edge size
+//      Pose anchorPose = centerAnchor.getPose();
+//
+//      float mazsScaleFactor = max_image_edge / maze_edge_size; // scale to set Maze to image size
+//      float[] modelMatrix = new float[16];
+//
+//      // OpenGL Matrix operation is in the order: Scale, rotation and Translation
+//      // So the manual adjustment is after scale
+//      // The 251.3f and 129.0f is magic number from the maze obj file
+//      // We need to do this adjustment because the maze obj file
+//      // is not centered around origin. Normally when you
+//      // work with your own model, you don't have this problem.
+//      Pose mozeModelLocalOffset = Pose.makeTranslation(
+//              262143.57f * mazsScaleFactor,
+//              -427295.75f * mazsScaleFactor,
+//              -218310.41f * mazsScaleFactor);
+//      anchorPose.compose(mozeModelLocalOffset).toMatrix(modelMatrix, 0);
+//      mazeRenderer.updateModelMatrix(modelMatrix, mazsScaleFactor,mazsScaleFactor,mazsScaleFactor);
+//      mazeRenderer.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+
+      final float teapot_edge_size = 132113.73f; // Magic number of teapot size
       final float max_image_edge = Math.max(augmentedImage.getExtentX(), augmentedImage.getExtentZ()); // Get largest detected image edge size
       Pose anchorPose = centerAnchor.getPose();
 
-      float mazsScaleFactor = max_image_edge / maze_edge_size; // scale to set Maze to image size
+      float teapotScaleFactor = max_image_edge / (teapot_edge_size*5); // scale to set Maze to image size
       float[] modelMatrix = new float[16];
 
-      // OpenGL Matrix operation is in the order: Scale, rotation and Translation
-      // So the manual adjustment is after scale
-      // The 251.3f and 129.0f is magic number from the maze obj file
-      // We need to do this adjustment because the maze obj file
-      // is not centered around origin. Normally when you
-      // work with your own model, you don't have this problem.
-      Pose mozeModelLocalOffset = Pose.makeTranslation(
-              262143.57f * mazsScaleFactor,
-              -427295.75f * mazsScaleFactor,
-              -218310.41f * mazsScaleFactor);
-      anchorPose.compose(mozeModelLocalOffset).toMatrix(modelMatrix, 0);
-      mazeRenderer.updateModelMatrix(modelMatrix, mazsScaleFactor,mazsScaleFactor,mazsScaleFactor);
-      mazeRenderer.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
-      Log.i("Tag", "loaded? " + mazsScaleFactor);
+//      //Rotate teapot
+//      float[] trans = {262143.57f * teapotScaleFactor + -0.3f * augmentedImage.getExtentX(),
+//              -427295.75f * teapotScaleFactor + 0.0f,
+//              -218310.41f * teapotScaleFactor + -0.4f * augmentedImage.getExtentZ()};
+//      float[] rotate = {262143.57f * teapotScaleFactor + -0.3f * augmentedImage.getExtentX(),
+//              -427295.75f * teapotScaleFactor + 0.0f,
+//              -218310.41f * teapotScaleFactor + -0.4f * augmentedImage.getExtentZ()};
+//      Pose teapotRotate = new Pose(trans, );
 
-//    Pose[] localBoundaryPoses = {
-//      Pose.makeTranslation(
-//          -0.5f * augmentedImage.getExtentX(),
-//          0.0f,
-//          -0.5f * augmentedImage.getExtentZ()), // upper left
-//      Pose.makeTranslation(
-//          0.5f * augmentedImage.getExtentX(),
-//          0.0f,
-//          -0.5f * augmentedImage.getExtentZ()), // upper right
-//      Pose.makeTranslation(
-//          0.5f * augmentedImage.getExtentX(),
-//          0.0f,
-//          0.5f * augmentedImage.getExtentZ()), // lower right
-//      Pose.makeTranslation(
-//          -0.5f * augmentedImage.getExtentX(),
-//          0.0f,
-//          0.5f * augmentedImage.getExtentZ()) // lower left
-//    };
-//
-//    Pose anchorPose = centerAnchor.getPose();
-//    Pose[] worldBoundaryPoses = new Pose[4];
-//    for (int i = 0; i < 4; ++i) {
-//      worldBoundaryPoses[i] = anchorPose.compose(localBoundaryPoses[i]);
-//    }
-//
-//    float scaleFactor = 1.0f;
-//    float[] modelMatrix = new float[16];
-//
-//    worldBoundaryPoses[0].toMatrix(modelMatrix, 0);
-//    imageFrameUpperLeft.updateModelMatrix(modelMatrix, scaleFactor);
-//    imageFrameUpperLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
-//
-//    worldBoundaryPoses[1].toMatrix(modelMatrix, 0);
-//    imageFrameUpperRight.updateModelMatrix(modelMatrix, scaleFactor);
-//    imageFrameUpperRight.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
-//
-//    worldBoundaryPoses[2].toMatrix(modelMatrix, 0);
-//    imageFrameLowerRight.updateModelMatrix(modelMatrix, scaleFactor);
-//    imageFrameLowerRight.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
-//
-//    worldBoundaryPoses[3].toMatrix(modelMatrix, 0);
-//    imageFrameLowerLeft.updateModelMatrix(modelMatrix, scaleFactor);
-//    imageFrameLowerLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+      Pose[] localBoundaryPoses = {
+          Pose.makeTranslation(
+                  262143.57f * teapotScaleFactor + -0.3f * augmentedImage.getExtentX(),
+                  -427295.75f * teapotScaleFactor + 0.0f,
+                  -218310.41f * teapotScaleFactor + -0.4f * augmentedImage.getExtentZ()),// upper left
+          Pose.makeTranslation(
+                  262143.57f * teapotScaleFactor + -0.10f * augmentedImage.getExtentX(),
+                  -427295.75f * teapotScaleFactor + 0.0f,
+                  -218310.41f * teapotScaleFactor + -0.4f * augmentedImage.getExtentZ()), // upper mid left
+          Pose.makeTranslation(
+                  262143.57f * teapotScaleFactor + 0.10f * augmentedImage.getExtentX(),
+                  -427295.75f * teapotScaleFactor + 0.0f,
+                  -218310.41f * teapotScaleFactor + -0.4f * augmentedImage.getExtentZ()), // upper mid right
+          Pose.makeTranslation(
+                  262143.57f * teapotScaleFactor + 0.3f * augmentedImage.getExtentX(),
+                  -427295.75f * teapotScaleFactor + 0.0f,
+                  -218310.41f * teapotScaleFactor + -0.4f * augmentedImage.getExtentZ()), // upper right
+        };
+      //Log.i("Tag", "scale is " + teapotScaleFactor);
+
+      Pose[] worldBoundaryPoses = new Pose[4];
+
+
+      for (int i = 0; i < 4; ++i) {
+          worldBoundaryPoses[i] = anchorPose.compose(localBoundaryPoses[i]);
+      }
+
+      worldBoundaryPoses[0].toMatrix(modelMatrix, 0);
+      teapotLeft.updateModelMatrix(modelMatrix, teapotScaleFactor, teapotScaleFactor, teapotScaleFactor);
+      teapotLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+
+      worldBoundaryPoses[1].toMatrix(modelMatrix, 0);
+      teapotMidLeft.updateModelMatrix(modelMatrix, teapotScaleFactor, teapotScaleFactor, teapotScaleFactor);
+      teapotMidLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+
+      worldBoundaryPoses[2].toMatrix(modelMatrix, 0);
+      teapotMidRight.updateModelMatrix(modelMatrix, teapotScaleFactor, teapotScaleFactor, teapotScaleFactor);
+      teapotMidRight.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+
+      worldBoundaryPoses[3].toMatrix(modelMatrix, 0);
+      teapotRight.updateModelMatrix(modelMatrix, teapotScaleFactor, teapotScaleFactor, teapotScaleFactor);
+      teapotRight.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
   }
 
   private static float[] convertHexToColor(int colorHex) {
